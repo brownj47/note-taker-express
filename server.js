abb6be5b5acac24fd82f04e269cb4ddcd0defefb
Justus B <justus.c.brown@gmail.com>
2022-07-23 14:57:00 -0700

    fetches work

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const PORT = process.env.PORT || 3000
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+app.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+app.get('/api/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'db/db.json'))
+})
+app.post('/api/notes', (req, res)=>{
+    console.log(req.body)
+
+    fs.readFile('./db/db.json')
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening on http://localhost:${PORT}`)
+})
